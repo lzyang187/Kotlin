3cfcb27bc3ce3531ec209c94f7de0ceb45ef6190
zyli44 <zyli44@iflytek.com>
2022-01-29 20:31:41 +0800

    feat:泛型代码更新

--- src/GenericDefine.kt
+++ src/GenericDefine.kt
@@ -1,3 +1,7 @@
+/**
+ * Java 中的泛型是不型变的，这意味着 List<String> 并不是 List<Object> 的子类型
+ */
+
 //泛型
 fun main(args: Array<String>) {
     val point = Point(1, 2)
@@ -8,7 +12,7 @@ fun main(args: Array<String>) {
 //泛型类
 class Point<T>(val x: T, val y: T)
 
-//声明处型变，由于它在类型参数声明处提供 out表示只能返回，不能作为函数的输入参数使用
+//声明处型变，由于它在类型参数声明处提供 out表示只能返回（生产），不能作为函数的输入参数使用（消费）
 interface Source<out T> {
     fun outT(): T
 }
@@ -18,6 +22,7 @@ fun demoOut(strSource: Source<String>) {
     val anySource: Source<Any> = strSource
 }
 
+// 类型参数逆变：只可以被消费而不可以被生产
 interface Comparable<in T> {
     fun compareTo(other: T): Int
 }
